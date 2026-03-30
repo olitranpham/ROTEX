30e8ea2a32b3aaaa6e15173e36f924efb3cfb41a
Kimberly Tice <kimberly_tice@student.uml.edu>
2026-03-29 21:46:52 -0400

    Add force calculation

--- Force_code.c
+++ Force_code.c
@@ -9,7 +9,7 @@ double epicondyle_accel_x = 4;
 double epicondyle_accel_y = 5;
 double epicondyle_accel_z = 6;
 double epicondyle_torque;
-float mass;
+float mass = 95;
 
 void main() {
     double net_acromion_accel = sqrt((acromion_accel_x * acromion_accel_x) + (acromion_accel_y * acromion_accel_y)+ (acromion_accel_z * acromion_accel_z));
@@ -17,4 +17,13 @@ void main() {
 
     double net_epicondyle_accel = sqrt((epicondyle_accel_x * epicondyle_accel_x) + (epicondyle_accel_y * epicondyle_accel_y) + (epicondyle_accel_z * epicondyle_accel_z));
     printf("Epicondyle net acceleration: %lf\n", net_epicondyle_accel);
+
+    double arm_mass = (mass * 0.453592 * 0.0575);
+    printf("Arm mass: %lf\n", arm_mass);
+
+    double net_acromion_force = (arm_mass * net_acromion_accel);
+    printf("Net acromion force: %lf\n", net_acromion_force);
+
+    double net_epicondyle_force = (arm_mass * net_epicondyle_accel);
+    printf("Net epicondyle force: %lf\n", net_epicondyle_force);
 }
