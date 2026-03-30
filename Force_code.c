55dd0f5b58a5331ce0edaa1ba78b763907d7162a
Kimberly Tice <kimberly_tice@student.uml.edu>
2026-03-30 13:22:00 -0400

    Add torque calculations

--- Force_code.c
+++ Force_code.c
@@ -4,26 +4,26 @@
 double acromion_accel_x = 1;
 double acromion_accel_y = 2;
 double acromion_accel_z = 3;
-double acromion_torque;
+double acromion_angular_vel = 7;
 double epicondyle_accel_x = 4;
 double epicondyle_accel_y = 5;
 double epicondyle_accel_z = 6;
-double epicondyle_torque;
+double epicondyle_angular_vel = 8;
 float mass = 95;
+double time_interval = 0.01;
 
 void main() {
-    double net_acromion_accel = sqrt((acromion_accel_x * acromion_accel_x) + (acromion_accel_y * acromion_accel_y)+ (acromion_accel_z * acromion_accel_z));
-    printf("Acromion net acceleration: %lf\n", net_acromion_accel);
-
-    double net_epicondyle_accel = sqrt((epicondyle_accel_x * epicondyle_accel_x) + (epicondyle_accel_y * epicondyle_accel_y) + (epicondyle_accel_z * epicondyle_accel_z));
-    printf("Epicondyle net acceleration: %lf\n", net_epicondyle_accel);
+    double net_accel = sqrt(((epicondyle_accel_x - acromion_accel_x) * (epicondyle_accel_x - acromion_accel_x)) + ((epicondyle_accel_y - acromion_accel_y) * (epicondyle_accel_y - acromion_accel_y)) + ((epicondyle_accel_z - acromion_accel_z) * (epicondyle_accel_z - acromion_accel_z)));
+    printf("Net acceleration: %lf\n", net_accel);
 
     double arm_mass = (mass * 0.453592 * 0.0575);
     printf("Arm mass: %lf\n", arm_mass);
 
-    double net_acromion_force = (arm_mass * net_acromion_accel);
-    printf("Net acromion force: %lf\n", net_acromion_force);
+    double net_force = (arm_mass * net_accel);
+    printf("Net force: %lf\n", net_force);
 
-    double net_epicondyle_force = (arm_mass * net_epicondyle_accel);
-    printf("Net epicondyle force: %lf\n", net_epicondyle_force);
+    double angular_vel = (epicondyle_angular_vel * 180 / 3.14159);
+    double angle = (angular_vel * time_interval);
+    double torque = fabs(net_force * 0.290 * sin(angle));
+    printf("Torque: %lf\n", torque);
 }
