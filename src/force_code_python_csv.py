0edb779d5c615623a977d83db0de59f30137f7f1
Kimberly Tice <kimberly_tice@student.uml.edu>
2026-04-08 10:45:48 -0400

    Update CSV file and fix reading CSV values

--- src/force_code_python_csv.py
+++ src/force_code_python_csv.py
@@ -3,20 +3,20 @@ import math
 import pandas           #note you will need to install pandas. Use command "pip install pandas" in terminal
 
 #User inputted variables
-max_torque = 0.2        #actual value 46.1
+max_torque = 2.3        #actual value 46.1
 mass = 95
 time_interval = 0.01    #remove and calculate time interval instead
 max_loop_iterations = 2 #sets how long the excessive torque timer lasts
 
 #Loops - do not touch
-iteration = 1
+iteration = 0
 flag = 0
 max_torque_flag = 0
 max_torque_timer = 0
 
 try:
     #read CSV file and find total number of columns
-    data = pandas.read_csv("src/sample_IMU_data.csv")
+    data = pandas.read_csv("src/SampleTorqueData.csv")
     total_iterations = len(data)
 
     #calculate arm mass
@@ -25,16 +25,16 @@ try:
 
     #get data from CSV for one specific time
     while (iteration < total_iterations): 
-        time_collected = data.iat[1, iteration]
-        acromion_accel_x = data.iat[2, iteration]
-        acromion_accel_y = data.iat[3, iteration]
-        acromion_accel_z = data.iat[4, iteration]
-        acromion_angular_vel = data.iat[5, iteration]
-        epicondyle_accel_x = data.iat[6, iteration]
-        epicondyle_accel_y = data.iat[7, iteration]
-        epicondyle_accel_z = data.iat[8, iteration]
-        epicondyle_angular_vel = data.iat[9, iteration]
-
+        time_collected = data.iat[iteration, 0]
+        acromion_accel_x = data.iat[iteration, 1]
+        acromion_accel_y = data.iat[iteration, 2]
+        acromion_accel_z = data.iat[iteration, 3]
+        acromion_angular_vel = data.iat[iteration, 4]
+        epicondyle_accel_x = data.iat[iteration, 5]
+        epicondyle_accel_y = data.iat[iteration, 6]
+        epicondyle_accel_z = data.iat[iteration, 7]
+        epicondyle_angular_vel = data.iat[iteration, 8]
+        
         #calculate net acceleration and net force
         net_accel = math.sqrt(((epicondyle_accel_x - acromion_accel_x) ** 2) + ((epicondyle_accel_y - acromion_accel_y) ** 2) + ((epicondyle_accel_z - acromion_accel_z) ** 2))
         print(f"Net acceleration: {net_accel}")
@@ -62,7 +62,7 @@ try:
             max_torque_timer = max_torque_timer - 1
 
         iteration = iteration + 1
-        time.sleep(1)
+        #time.sleep(.25)
 
 except KeyboardInterrupt:       #ctrl + c
     print("Error. Testing concluded.")
